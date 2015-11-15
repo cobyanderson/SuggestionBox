7a3e9f1c0b50eefddb2060d2693ba3b468c05808
cobyanderson <samuelcobyanderson@gmail.com>
2015-11-14 19:00:48 -0800

    Initial Commit

---
+++ SuggestionBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuggestionBox
+//
+//  Created by Samuel Coby Anderson on 11/14/15.
+//  Copyright © 2015 Samuel Coby Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
